d2a8d30664d6d88caaa6ec38fd0448b21fc8085c
MayBaMay <maylis.baschet@gmail.com>
2020-03-30 17:21:46 +0200

    static

--- pur_beurre_project/settings/__init__.py
+++ pur_beurre_project/settings/__init__.py
@@ -133,13 +133,9 @@ INTERNAL_IPS = ['127.0.0.1']
 LOGOUT_REDIRECT_URL = '/'
 
 if os.environ.get('ENV') == 'PRODUCTION':
-    PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-    STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-    STATICFILES_DIRS = (
-        os.path.join(PROJECT_ROOT, 'static'),
-    )
 
+    STATIC_ROOT = os.path.join(BASE_ROOT, 'staticfiles')
+
+    # Simplified static file serving.
+    # https://warehouse.python.org/project/whitenoise/
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-    db_from_env = dj_database_url.config(conn_max_age=500)
-    DATABASES['default'].update(db_from_env)
