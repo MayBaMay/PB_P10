943684aecc748e635c64d85e030fc64a7d8efdfe
MayBaMay <maylis.baschet@gmail.com>
2020-04-07 18:09:37 +0200

    settings db local

--- pur_beurre_project/settings/__init__.py
+++ pur_beurre_project/settings/__init__.py
@@ -27,7 +27,7 @@ SECRET_KEY = '7l*om2l-k#b0ec)e(5_3+z)m4p%9fgh3p*t$(f93hgxjaftr@6'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'pb',
+        'NAME': 'purbeurreprod',
         'USER': 'maylisbaschet',
         'PASSWORD': '',
         'HOST': '',
