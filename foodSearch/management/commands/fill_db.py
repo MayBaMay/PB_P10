4aa453ef427ecdfe5c7d326f09b4c5fa73e2bc80
MayBaMay <maylis.baschet@gmail.com>
2020-04-07 18:01:19 +0200

    update database command ok

--- foodSearch/management/commands/fill_db.py
+++ foodSearch/management/commands/fill_db.py
@@ -10,10 +10,9 @@ import time
 import datetime
 from statistics import mean
 import unicodedata
-import json
 
 from django.core.management.base import BaseCommand
-from django.db import transaction, IntegrityError
+from django.db import transaction
 
 import openfoodfacts
 
@@ -43,73 +42,77 @@ class InitDB:
     def upper_unaccent(sentence):
         """"return a string in upper case and without any accent"""
         try:
-            sentence_unaccent = ''.join(
-                (c for c in unicodedata.normalize('NFD', sentence)
-                 if unicodedata.category(c) != 'Mn'))
+            sentence_unaccent = "".join(
+                (c for c in unicodedata.normalize("NFD", sentence)
+                 if unicodedata.category(c) != "Mn"))
             return sentence_unaccent.upper()
         except:
             return sentence
 
-    def load_product(self, product):
-
-        product_infos = {
-                         'required':{'error':False},
-                         'optional':{}
-                         }
-
-        try:
-            # required informations
-            product_infos['required']['reference'] = product["id"],
-            product_infos['required']['name'] = product["product_name"]
-            product_infos['required']['formatted_name'] = self.upper_unaccent(product['product_name'])
-            product_infos['required']['brands'] = product['brands']
-            product_infos['required']['formatted_brands'] = self.upper_unaccent(product['brands'])
-            product_infos['required']['nutrition_grade_fr'] = product["nutrition_grades"]
-            product_infos['required']['url'] = product["url"]
-            product_infos['required']['image_url'] = product["image_url"]
-            product_infos['required']['image_small_url'] = product["image_small_url"]
-
-        except KeyError:
-            product_infos['required']['error'] = True #keep only complete product
-
-        if not product_infos['required']['error']:
-            # optional informations
-
-            try:
-                product_infos['optional']['saturated_fat_100g'] = product['nutriments']['saturated-fat_100g']
-            except KeyError:
-                pass
-            try:
-                product_infos['optional']['carbohydrates_100g'] = product['nutriments']['carbohydrates_100g']
-            except KeyError:
-                pass
-            try:
-                product_infos['optional']['energy_100g'] = product['nutriments']['energy_100g']
-            except KeyError:
-                pass
-            try:
-                product_infos['optional']['sugars_100g'] = product['nutriments']['sugars_100g']
-            except KeyError:
-                pass
-            try:
-                product_infos['optional']['sodium_100g'] = product['nutriments']['sodium_100g']
-            except KeyError:
-                pass
-            try:
-                product_infos['optional']['salt_100g'] = product['nutriments']['salt_100g']
-            except KeyError:
-                pass
-
-        return product_infos
-
     def load_off_page(self):
+        """load page of products from openfactfood"""
         print("\n")
         print("loading page "+ str(self.page))
         # use openfactfood api to load pages
         page_prods = openfoodfacts.products.get_by_facets(
-            {'country': 'france'}, page=self.page, locale="fr"
+            {"country": "france"}, page=self.page, locale="fr"
         )
         return page_prods
+
+    def load_product(self, off_json):
+        """
+        load product's infos from openfactfood page of products into a dictionnary
+        devided between required fields and optionnal fields
+        """
+        product_infos = {
+            "required":{"error":False},
+            "optional":{}
+            }
+
+        try:
+            # required informations
+            product_infos["required"]["reference"] = off_json["id"]
+            product_infos["required"]["name"] = off_json["product_name"]
+            product_infos["required"]["formatted_name"] = self.upper_unaccent(off_json["product_name"])
+            product_infos["required"]["brands"] = off_json["brands"]
+            product_infos["required"]["formatted_brands"] = self.upper_unaccent(off_json["brands"])
+            product_infos["required"]["nutrition_grade_fr"] = off_json["nutrition_grades"]
+            product_infos["required"]["url"] = off_json["url"]
+            product_infos["required"]["image_url"] = off_json["image_url"]
+            product_infos["required"]["image_small_url"] = off_json["image_small_url"]
+
+        except KeyError:
+            product_infos["required"]["error"] = True #keep only complete product
+
+        if not product_infos["required"]["error"]:
+            # optional informations
+
+            try:
+                product_infos["optional"]["saturated_fat_100g"] = off_json["nutriments"]["saturated-fat_100g"]
+            except KeyError:
+                pass
+            try:
+                product_infos["optional"]["carbohydrates_100g"] = off_json["nutriments"]["carbohydrates_100g"]
+            except KeyError:
+                pass
+            try:
+                product_infos["optional"]["energy_100g"] = off_json["nutriments"]["energy_100g"]
+            except KeyError:
+                pass
+            try:
+                product_infos["optional"]["sugars_100g"] = off_json["nutriments"]["sugars_100g"]
+            except KeyError:
+                pass
+            try:
+                product_infos["optional"]["sodium_100g"] = off_json["nutriments"]["sodium_100g"]
+            except KeyError:
+                pass
+            try:
+                product_infos["optional"]["salt_100g"] = off_json["nutriments"]["salt_100g"]
+            except KeyError:
+                pass
+
+        return product_infos
 
     def load_datas(self, page, last_page):
         """method loading datas from api in the database"""
@@ -118,51 +121,19 @@ class InitDB:
         self.page = page # page counter
         self.last_page = last_page # number of page wanted from the api
         start_time = time.time()
-        n=0
 
         while self.page <= self.last_page:
             page_prods = self.load_off_page()
 
-            for product in page_prods:
-                n+=1
-                product_infos = self.load_product(product)
+            for off_json in page_prods:
+                product_infos = self.load_product(off_json)
 
-                if not product_infos['required']['error']:
+                if not product_infos["required"]["error"]:
 
-                    with transaction.atomic():
-                        # insert each product in database
-
-                        try:
-                            new_product = Product.objects.create(
-                                reference=product_infos['required']["reference"],
-                                name=product_infos['required']['name'],
-                                formatted_name=product_infos['required']['formatted_name'],
-                                brands=product_infos['required']['brands'],
-                                formatted_brands=product_infos['required']['formatted_brands'],
-                                nutrition_grade_fr=product_infos['required']["nutrition_grade_fr"],
-                                url=product_infos['required']["url"],
-                                image_url=product_infos['required']["image_url"],
-                                image_small_url=product_infos['required']["image_small_url"],
-                            )
-                            Product.objects.filter(pk=new_product.id).update(**product_infos['optional'])
-
-                            # insert each category in database only if products
-                            # has categories infos(no keyerror in product['categories_hierarchy'])
-                            with transaction.atomic():
-                                for category in product['categories_hierarchy']:
-                                    if category[0:3] == 'en:':
-                                        try:
-                                            # try to get the category in database
-                                            cat = Category.objects.get(reference=category)
-                                        except Category.DoesNotExist:
-                                            # if category doesn't exist yet, create one
-                                            cat = Category.objects.create(reference=category)
-                                            # categ = Category.objects.get(reference=category)
-                                        # in any case, add a relation between Category and Product
-                                        cat.products.add(new_product)
-                        ###### only keep cleaned datas #######
-                        except IntegrityError:
-                            pass
+                    if Product.objects.filter(name=product_infos["required"]["name"]).exists():
+                        self.update_product(off_json, product_infos)
+                    else:
+                        self.add_product(off_json, product_infos)
 
             print("{} products in database".format(Product.objects.count()))
             print("{} categories in database".format(Category.objects.count()))
@@ -171,8 +142,85 @@ class InitDB:
             self.tps.append(tps_page)
             self.page += 1
 
-    def update_datas(self):
-        pass
+    @staticmethod
+    def keep_eng_categories(off_json):
+        """
+        keep only relevant categories from openfactfood datas
+        Even for french products, categories are mostly in english
+        """
+        parsed_categories = []
+        for category in off_json["categories_hierarchy"]:
+            if category[0:3] == "en:":
+                parsed_categories.append(category)
+        return parsed_categories
+
+    @staticmethod
+    def update_categories(off_json, product):
+        """
+        Create if needed a new category &
+        Update M2M relation between the produt and categories
+        """
+        # insert each category in database only if products
+        # has categories infos(no keyerror in product["categories_hierarchy"])
+        with transaction.atomic():
+            try:
+                for category in keep_eng_categories(off_json):
+                    if category[0:3] == "en:":
+                        try:
+                            # try to get the category in database
+                            cat = Category.objects.get(reference=category)
+                        except Category.DoesNotExist:
+                            # if category doesn"t exist yet, create one
+                            cat = Category.objects.create(reference=category)
+                            # categ = Category.objects.get(reference=category)
+                        # in any case, add a relation between Category and Product
+                        cat.products.add(product)
+            ###### only keep cleaned datas #######
+            except:
+                pass
+
+    def add_product(self, off_json, product_infos):
+        """
+        create a new product in database
+        """
+        with transaction.atomic():
+            # insert each product in database
+
+            try:
+                new_product = Product.objects.create(
+                    reference=product_infos["required"]["reference"],
+                    name=product_infos["required"]["name"],
+                    formatted_name=product_infos["required"]["formatted_name"],
+                    brands=product_infos["required"]["brands"],
+                    formatted_brands=product_infos["required"]["formatted_brands"],
+                    nutrition_grade_fr=product_infos["required"]["nutrition_grade_fr"],
+                    url=product_infos["required"]["url"],
+                    image_url=product_infos["required"]["image_url"],
+                    image_small_url=product_infos["required"]["image_small_url"],
+                )
+                Product.objects.filter(pk=new_product.id).update(**product_infos["optional"])
+
+                self.update_categories(off_json,
+                                       new_product)
+
+            ###### only keep cleaned datas #######
+            except:
+                pass
+
+    def update_product(self, off_json, product_infos):
+        """
+        Update product's informations
+        """
+        product = Product.objects.filter(name=product_infos["required"]["name"])
+        del product_infos["required"]["error"]
+        product.update(**product_infos["required"])
+        product.update(**product_infos["optional"])
+
+        product = Product.objects.get(name=product_infos["required"]["name"])
+        parsed_categories = self.keep_eng_categories(off_json)
+        if len(parsed_categories) > Category.objects.filter(products__id=product.id).count():
+            product.categories.clear()
+            self.update_categories(off_json, product)
 
 
 class Command(BaseCommand):
@@ -180,21 +228,21 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         """Class arguments : reset or fill"""
-        parser.add_argument('-r',
-                            '--reset',
-                            action='store_true',
-                            dest='reset',
-                            help='Reset database')
-        parser.add_argument('-f',
-                            '--fill',
-                            action='store_true',
-                            dest='fill',
-                            help='Fill database')
+        parser.add_argument("-r",
+                            "--reset",
+                            action="store_true",
+                            dest="reset",
+                            help="Reset database")
+        parser.add_argument("-f",
+                            "--fill",
+                            action="store_true",
+                            dest="fill",
+                            help="Fill database")
 
     def handle(self, **options):
         """Class handler, launch reset or fill depending on option choice"""
 
-        if options['reset']:
+        if options["reset"]:
             database = InitDB()
             database.reset_db()
 
@@ -204,7 +252,7 @@ class Command(BaseCommand):
 
             self.stdout.write(self.style.SUCCESS("Reset base de données effectué"))
 
-        if options['fill']:
+        if options["fill"]:
             products = Product.objects.count()
             categories = Category.objects.count()
 
@@ -217,7 +265,7 @@ class Command(BaseCommand):
             --- Database FILLED from page {} to {}
             --- {} products in database ({} added)
             --- {} categories in database ({} added)
-            --- Temps moyen d'execution : {} secondes par page ---\n"""
+            --- Temps moyen d"execution : {} secondes par page ---\n"""
                        .format(datetime.datetime.now(),
                                database.initial_page,
                                database.last_page,
