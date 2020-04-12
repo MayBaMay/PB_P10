deea8b784c339e18dbdad913a30fd54790ee84bb
MayBaMay <maylis.baschet@gmail.com>
2020-04-12 10:07:17 +0200

    settings travis bug import settings default

--- pur_beurre_project/settings/__init__.py
+++ pur_beurre_project/settings/__init__.py
@@ -17,17 +17,7 @@ import dj_database_url
 try:
     from .local import SECRET_KEY, DATABASES
 except:
-    SECRET_KEY = '7l*om2l-k#b0ec)e(5_3+z)m4p%9fgh3p*t$(f93hgxjaftr@6'
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.postgresql',
-            'NAME': '',
-            'USER': 'postgres',
-            'PASSWORD': '',
-            'HOST': '',
-            'PORT': '',
-        }
-    }
+    pass
 
 DEBUG = True
 ALLOWED_HOSTS = []
