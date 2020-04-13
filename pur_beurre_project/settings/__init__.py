92b825983f3c2116edd299a214fcf89bd014b2af
MayBaMay <maylis.baschet@gmail.com>
2020-04-13 15:38:23 +0200

    __init__ settings

--- pur_beurre_project/settings/__init__.py
+++ pur_beurre_project/settings/__init__.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 import os
 import dj_database_url
 
+
 try:
     from .local import SECRET_KEY, DATABASES
 except:
@@ -20,6 +21,7 @@ except:
 
 DEBUG = True
 ALLOWED_HOSTS = []
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
