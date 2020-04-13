8a8c70e533774e0cdf827f0f5979791f8ca61d89
MayBaMay <maylis.baschet@gmail.com>
2020-04-13 13:18:05 +0200

    settings prod

--- pur_beurre_project/settings/__init__.py
+++ pur_beurre_project/settings/__init__.py
@@ -19,10 +19,9 @@ try:
 except:
     pass
 
-try:
-    from .production import *
-except:
-    pass
+DEBUG = True
+ALLOWED_HOSTS = []
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
