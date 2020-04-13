97f16246d131a15569d4e167e4cc2757ffe8d740
MayBaMay <maylis.baschet@gmail.com>
2020-04-13 18:18:36 +0200

    try .production

--- pur_beurre_project/settings/__init__.py
+++ pur_beurre_project/settings/__init__.py
@@ -30,6 +30,11 @@ DATABASES = {
     }
 }
 
+try:
+    from .production import *
+except:
+    pass  
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
