13a0151b23d13abda0ec649cd104b152f2a2ee1f
MayBaMay <maylis.baschet@gmail.com>
2020-04-15 14:31:50 +0200

    refacto porduction.py

--- pur_beurre_project/settings/production.py
+++ pur_beurre_project/settings/production.py
@@ -1,16 +1,15 @@
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
-
-sentry_sdk.init(
-    dsn="https://2e760245c52d4ff7bd9ece56a3129bc7@sentry.io/5184701",
-    integrations=[DjangoIntegration()],
-)
+import raven
 
 from . import *
 
 DEBUG = False
 
-import raven
+sentry_sdk.init(
+    dsn="https://2e760245c52d4ff7bd9ece56a3129bc7@sentry.io/5184701",
+    integrations=[DjangoIntegration()],
+)
 
 INSTALLED_APPS += [
     'raven.contrib.django.raven_compat',
