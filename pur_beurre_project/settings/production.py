6686530144ede5b70e9a9efb24e307b9495ebe22
MayBaMay <maylis.baschet@gmail.com>
2020-04-16 14:07:37 +0200

    sentry commenté pour test déploiement total

--- pur_beurre_project/settings/production.py
+++ pur_beurre_project/settings/production.py
@@ -1,11 +1,11 @@
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+# import sentry_sdk
+# from sentry_sdk.integrations.django import DjangoIntegration
 
 from . import *
 
 DEBUG = False
 
-sentry_sdk.init(
-    dsn="https://2e760245c52d4ff7bd9ece56a3129bc7@sentry.io/5184701",
-    integrations=[DjangoIntegration()],
-)
+# sentry_sdk.init(
+#     dsn="https://2e760245c52d4ff7bd9ece56a3129bc7@sentry.io/5184701",
+#     integrations=[DjangoIntegration()],
+# )
