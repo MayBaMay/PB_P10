d4804939db4cc126d65a2f57e27d50d33c4ec4eb
MayBaMay <maylis.baschet@gmail.com>
2020-04-16 22:05:53 +0200

    production sans sentry

--- pur_beurre_project/settings/production.py
+++ pur_beurre_project/settings/production.py
@@ -1,11 +1,3 @@
-# import sentry_sdk
-# from sentry_sdk.integrations.django import DjangoIntegration
-
 from . import *
 
 DEBUG = False
-
-# sentry_sdk.init(
-#     dsn="https://2e760245c52d4ff7bd9ece56a3129bc7@sentry.io/5184701",
-#     integrations=[DjangoIntegration()],
-# )
