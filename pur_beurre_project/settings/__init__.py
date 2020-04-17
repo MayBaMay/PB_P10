30fa154ba65995bd67991c5f3f867ce970f9ae61
MayBaMay <maylis.baschet@gmail.com>
2020-04-17 15:05:42 +0200

    test eeror in views & sentry in settings __init__

--- pur_beurre_project/settings/__init__.py
+++ pur_beurre_project/settings/__init__.py
@@ -10,6 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
+sentry_sdk.init(
+    dsn="https://57df46d460e3492997462bddc1118433@o371148.ingest.sentry.io/5203994",
+    integrations=[DjangoIntegration()],
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
 
 import os
 import dj_database_url
